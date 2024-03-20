0622989402c2e512a6f158585d2bc24e4367b97c
Joshua Si <joshuasi777@gmail.com>
2024-03-20 00:20:50 -0700

    Fix calculateBalancedGroups & add printFamiliarity

--- balanced_group_system/balanced_group_system.py
+++ balanced_group_system/balanced_group_system.py
@@ -33,38 +33,57 @@ class BalancedGroupSystem(group_system.GroupSystem):
 			row.pop(index)
 		self.familiarityMatrix.pop(index)
 		
-	def createGroups(self, groups: list[list[str]]) -> None:
+	def createGroups(self, groups: list[list[str]]) -> list[list[str]]:
 		super().createGroups(groups)
 		for group in groups:
-			for i in group:
-				for j in group:
-					if i != j:
-						self.familiarityMatrix[self.members.index(j)][self.members.index(i)] += 1
+			self.updateFamiliarity(group)
 		return groups
 
-	def printMatrix(self) -> None:
-		for row in self.familiarityMatrix:
-			print(row)
+	def printFamiliarity(self) -> None:
+		print('  ',', '.join(self.members))
+		for i, row in enumerate(self.familiarityMatrix):
+			print(self.members[i], row)
 
 	def evaluateGroup(self, group: list[str]) -> int:
 		score = 0
 		for i, j in enumerate(group):
 			for k in group[i+1:]:
-				self.familiarityMatrix[self.members.index(j)][self.members.index(k)] += 1
-				self.familiarityMatrix[self.members.index(k)][self.members.index(j)] += 1
 				score += self.familiarityMatrix[self.members.index(j)][self.members.index(k)]
 		return score
+	
+	def updateFamiliarity(self, group: list[str]) -> None:
+		for i in range(len(group)):
+			member_i = self.members.index(group[i])	
+			for j in group[i+1:]:
+				member_j = self.members.index(j)
+				self.familiarityMatrix[member_j][member_i] += 1
+				self.familiarityMatrix[member_i][member_j] += 1
 
 	def calculateBalancedGroups(self, groupCount: int, members: list[str] = [], verbose: bool = False) -> list[list[str]]:
 		random.shuffle(members)
 		groups = [[] for _ in range(groupCount)]
+		scores = {(): 0}
+		# Memoized version of evaluateGroup for evaluating score when adding one member
+		def evaluateMember(group, member):
+			group_key = tuple(sorted(group))
+			new_key = tuple(sorted(group+[member]))
+			if new_key in scores:
+				return scores[new_key]
+			score = scores[group_key]
+			for i in group:
+				score += self.familiarityMatrix[self.members.index(i)][self.members.index(member)]
+			scores[new_key] = score + 1
+			return score + 1
+
+		# Balance and distribute members
 		for member in members:
-			min_group_index = min(range(groupCount), key=lambda i: self.evaluateGroup(groups[i] + [member]))
+			min_group_index = min(range(groupCount), key=lambda i: evaluateMember(groups[i], member))
 			groups[min_group_index].append(member)
 			if verbose:
 				print(groups)
 		return groups
 
-	def createBalancedGroups(self, groupCount: int) -> None:
+
+	def createBalancedGroups(self, groupCount: int) -> list[list[str]]:
 		groups = self.calculateBalancedGroups(groupCount, self.members.copy())
 		return self.createGroups(groups)
